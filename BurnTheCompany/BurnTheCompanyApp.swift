42d38b9b1775fc432f1e8ce406d28eee1c52b6fd
Arpit <Arpit980jaiswal@gmail.com>
2024-02-09 00:37:59 +0530

    Hey Updated new stack

---
+++ BurnTheCompany/BurnTheCompanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnTheCompanyApp.swift
+//  BurnTheCompany
+//
+//  Created by Arpit Jaiswal on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BurnTheCompanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
